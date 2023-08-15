76a16a01b65e272b73fd526234a531232e095c38
SKLee <steelblue.lee@gmail.com>
2023-08-15 22:52:08 +0900

    2023-08-15-22-52-08

--- nvim/lua/c_common/luasnip.lua
+++ nvim/lua/c_common/luasnip.lua
@@ -1,2 +1,6 @@
 require("luasnip.loaders.from_vscode").lazy_load()
 require("luasnip.loaders.from_vscode").lazy_load({ paths = { os.getenv("HOME") .. "/.config/nvim/my-snippets" } })
+
+require("luasnip").config.set_config({
+	enable_autosnippets = true,
+})
