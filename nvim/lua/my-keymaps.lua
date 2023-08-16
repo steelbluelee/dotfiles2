e8a1a02e93aae0b58090b284ead6038b7ed2ceee
SKLee <steelblue.lee@gmail.com>
2023-08-16 16:56:35 +0900

    2023-08-16-16-56-35

--- nvim/lua/my-keymaps.lua
+++ nvim/lua/my-keymaps.lua
@@ -1,6 +1,6 @@
 vim.cmd([[
     augroup ForJavaSpringBoot
       autocmd!
-      autocmd FileType java,groovy,html,html.mustache nnoremap <buffer> <C-s><C-b> :write<CR>:1TermExec cmd="kill -9 $(jobs -p);./gradlew bootRun < /dev/null &"<CR>
+      autocmd FileType java,groovy,html,html.mustache nnoremap <buffer> <C-s><C-r> :write<CR>:1TermExec cmd="kill -9 $(jobs -p);./gradlew bootRun < /dev/null &"<CR>
     augroup end
   ]])
