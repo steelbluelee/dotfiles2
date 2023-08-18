5bea6b30cdd861017d6e6e4def3757e2ad5dd665
SeungKyu Lee <steelblue.lee@gmail.com>
2023-08-18 11:07:24 +0900

    2023-08-18-11-07-24

--- nvim/lua/b_programming/null-ls.lua
+++ nvim/lua/b_programming/null-ls.lua
@@ -6,6 +6,12 @@ null_ls.setup({
 		null_ls.builtins.formatting.prettier,
 		null_ls.builtins.formatting.stylua,
 		null_ls.builtins.formatting.dart_format,
+		null_ls.builtins.formatting.djlint.with({
+			filetypes = { "html.mustache" },
+		}),
+		null_ls.builtins.diagnostics.djlint.with({
+			filetypes = { "html.mustache" },
+		}),
 		-- bash
 		null_ls.builtins.code_actions.shellcheck,
 		null_ls.builtins.diagnostics.shellcheck,
