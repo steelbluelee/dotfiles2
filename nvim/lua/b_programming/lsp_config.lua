75a775f1c5c968dee6de9078a2f9f75499263960
SKLee <steelblue.lee@gmail.com>
2024-09-25 11:04:41 +0900

    2024-09-25-11-04-41

--- nvim/lua/b_programming/lsp_config.lua
+++ nvim/lua/b_programming/lsp_config.lua
@@ -5,30 +5,31 @@ lsp.bashls.setup({})
 require("flutter-tools").setup({}) -- use defaults
 lsp.dartls.setup({})
 
-lsp.tsserver.setup({})
+-- lsp.tsserver.setup({})
+lsp.ts_ls.setup({})
 
 lsp.lua_ls.setup({
-	settings = {
-		Lua = {
-			runtime = {
-				-- Tell the language server which version of Lua you're using (most likely LuaJIT in the case of Neovim)
-				version = "LuaJIT",
-			},
-			diagnostics = {
-				-- Get the language server to recognize the `vim` global
-				globals = { "vim" },
-			},
-			workspace = {
-				-- Make the server aware of Neovim runtime files
-				library = vim.api.nvim_get_runtime_file("", true),
-				checkThirdParty = false, -- THIS IS THE IMPORTANT LINE TO ADD
-			},
-			-- Do not send telemetry data containing a randomized but unique identifier
-			telemetry = {
-				enable = false,
-			},
-		},
-	},
+  settings = {
+    Lua = {
+      runtime = {
+        -- Tell the language server which version of Lua you're using (most likely LuaJIT in the case of Neovim)
+        version = "LuaJIT",
+      },
+      diagnostics = {
+        -- Get the language server to recognize the `vim` global
+        globals = { "vim" },
+      },
+      workspace = {
+        -- Make the server aware of Neovim runtime files
+        library = vim.api.nvim_get_runtime_file("", true),
+        checkThirdParty = false, -- THIS IS THE IMPORTANT LINE TO ADD
+      },
+      -- Do not send telemetry data containing a randomized but unique identifier
+      telemetry = {
+        enable = false,
+      },
+    },
+  },
 })
 --
 require("lspconfig").jdtls.setup({})
--- nvim/lua/c_common/telescope.lua
+++ nvim/lua/c_common/telescope.lua
@@ -6,21 +6,21 @@ vim.keymap.set("n", "<leader>fh", builtin.help_tags, { noremap = true })
 -- You don't need to set any of these options.
 -- IMPORTANT!: this is only a showcase of how you can set default options!
 require("telescope").setup({
-	extensions = {
-		file_browser = {
-			theme = "ivy",
-			-- disables netrw and use telescope-file-browser in its place
-			hijack_netrw = true,
-			mappings = {
-				["i"] = {
-					-- your custom insert mode mappings
-				},
-				["n"] = {
-					-- your custom normal mode mappings
-				},
-			},
-		},
-	},
+  extensions = {
+    file_browser = {
+      theme = "ivy",
+      -- disables netrw and use telescope-file-browser in its place
+      hijack_netrw = true,
+      mappings = {
+        ["i"] = {
+          -- your custom insert mode mappings
+        },
+        ["n"] = {
+          -- your custom normal mode mappings
+        },
+      },
+    },
+  },
 })
 -- To get telescope-file-browser loaded and working with telescope,
 -- you need to call load_extension, somewhere after setup function:
@@ -30,15 +30,18 @@ vim.keymap.set("n", "<space>fb", ":Telescope file_browser<CR>", { noremap = true
 
 -- for trouble.nvim
 local actions = require("telescope.actions")
-local trouble = require("trouble.providers.telescope")
+-- local trouble = require("trouble.providers.telescope")
+local trouble = require("trouble.sources.telescope")
 
 local telescope = require("telescope")
 
 telescope.setup({
-	defaults = {
-		mappings = {
-			i = { ["<c-t>"] = trouble.open_with_trouble },
-			n = { ["<c-t>"] = trouble.open_with_trouble },
-		},
-	},
+  defaults = {
+    mappings = {
+      --[[ i = { ["<c-t>"] = trouble.open_with_trouble },
+      n = { ["<c-t>"] = trouble.open_with_trouble }, ]]
+      i = { ["<c-t>"] = trouble.open },
+      n = { ["<c-t>"] = trouble.open },
+    },
+  },
 })
