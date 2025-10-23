41830a60debcbf1abbbfce3350e84af44188ecff
sklee <steelblue.lee@gmail.com>
2025-10-23 15:23:50 +0900

    2025-10-23-15-23-50

--- nvim/lua/b_programming/lsp_config.lua
+++ nvim/lua/b_programming/lsp_config.lua
@@ -1,35 +1,37 @@
 local lsp = require("lspconfig")
+-- local l = vim.lsp.config
 
 lsp.bashls.setup({})
+-- l.bashls.setup({})
 
 require("flutter-tools").setup({}) -- use defaults
-lsp.dartls.setup({})
+-- lsp.dartls.setup({})
 
 -- lsp.tsserver.setup({})
 lsp.ts_ls.setup({})
 
 lsp.lua_ls.setup({
-  settings = {
-    Lua = {
-      runtime = {
-        -- Tell the language server which version of Lua you're using (most likely LuaJIT in the case of Neovim)
-        version = "LuaJIT",
-      },
-      diagnostics = {
-        -- Get the language server to recognize the `vim` global
-        globals = { "vim" },
-      },
-      workspace = {
-        -- Make the server aware of Neovim runtime files
-        library = vim.api.nvim_get_runtime_file("", true),
-        checkThirdParty = false, -- THIS IS THE IMPORTANT LINE TO ADD
-      },
-      -- Do not send telemetry data containing a randomized but unique identifier
-      telemetry = {
-        enable = false,
-      },
-    },
-  },
+	settings = {
+		Lua = {
+			runtime = {
+				-- Tell the language server which version of Lua you're using (most likely LuaJIT in the case of Neovim)
+				version = "LuaJIT",
+			},
+			diagnostics = {
+				-- Get the language server to recognize the `vim` global
+				globals = { "vim" },
+			},
+			workspace = {
+				-- Make the server aware of Neovim runtime files
+				library = vim.api.nvim_get_runtime_file("", true),
+				checkThirdParty = false, -- THIS IS THE IMPORTANT LINE TO ADD
+			},
+			-- Do not send telemetry data containing a randomized but unique identifier
+			telemetry = {
+				enable = false,
+			},
+		},
+	},
 })
 --
 require("lspconfig").jdtls.setup({})
