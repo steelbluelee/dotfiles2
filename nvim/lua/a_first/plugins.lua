236aca39bcf078758f7f863a42c4384695cba021
SKLee <steelblue.lee@gmail.com>
2026-05-21 14:25:00 +0900

    2026-05-21-14-25-00

--- nvim/lua/a_first/plugins.lua
+++ nvim/lua/a_first/plugins.lua
@@ -1,150 +1,169 @@
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not vim.loop.fs_stat(lazypath) then
-	vim.fn.system({
-		"git",
-		"clone",
-		"--filter=blob:none",
-		"https://github.com/folke/lazy.nvim.git",
-		"--branch=stable", -- latest stable release
-		lazypath,
-	})
+  vim.fn.system({
+    "git",
+    "clone",
+    "--filter=blob:none",
+    "https://github.com/folke/lazy.nvim.git",
+    "--branch=stable", -- latest stable release
+    lazypath,
+  })
 end
 vim.opt.rtp:prepend(lazypath)
 require("lazy").setup({
-	--
-	{
-		"williamboman/mason.nvim",
-		config = function()
-			require("mason").setup()
-		end,
-	},
-	"williamboman/mason-lspconfig.nvim",
-	{
-		"jay-babu/mason-null-ls.nvim",
-		event = { "BufReadPre", "BufNewFile" },
-		dependencies = {
-			"williamboman/mason.nvim",
-			"nvimtools/none-ls.nvim",
-		},
-		-- config = function()
-		-- require("your.null-ls.config") -- require your null-ls config here (example below)
-		-- end,
-	},
-	"nvimtools/none-ls.nvim",
+  --
+  {
+    "williamboman/mason.nvim",
+    config = function()
+      require("mason").setup()
+    end,
+  },
+  "williamboman/mason-lspconfig.nvim",
+  {
+    "jay-babu/mason-null-ls.nvim",
+    event = { "BufReadPre", "BufNewFile" },
+    dependencies = {
+      "williamboman/mason.nvim",
+      "nvimtools/none-ls.nvim",
+    },
+    -- config = function()
+    -- require("your.null-ls.config") -- require your null-ls config here (example below)
+    -- end,
+  },
+  "nvimtools/none-ls.nvim",
 
-	"neovim/nvim-lspconfig",
+  "neovim/nvim-lspconfig",
 
-	"folke/tokyonight.nvim",
-	{
-		"nvim-lualine/lualine.nvim",
-		dependencies = { "nvim-tree/nvim-web-devicons", opt = true },
-	},
+  "folke/tokyonight.nvim",
+  {
+    "nvim-lualine/lualine.nvim",
+    dependencies = { "nvim-tree/nvim-web-devicons", opt = true },
+  },
 
-	-- { "akinsho/bufferline.nvim", version = "*", dependencies = "nvim-tree/nvim-web-devicons" },
-	{
-		"kdheepak/tabline.nvim",
-		dependencies = { { "nvim-lualine/lualine.nvim", opt = true }, { "nvim-tree/nvim-web-devicons", opt = true } },
-	},
+  -- { "akinsho/bufferline.nvim", version = "*", dependencies = "nvim-tree/nvim-web-devicons" },
+  {
+    "kdheepak/tabline.nvim",
+    dependencies = { { "nvim-lualine/lualine.nvim", opt = true }, { "nvim-tree/nvim-web-devicons", opt = true } },
+  },
 
-	{
-		"nvim-tree/nvim-tree.lua",
-		version = "*",
-		lazy = false,
-		dependencies = {
-			"nvim-tree/nvim-web-devicons",
-		},
-		config = function()
-			require("nvim-tree").setup({})
-		end,
-	},
+  {
+    "nvim-tree/nvim-tree.lua",
+    version = "*",
+    lazy = false,
+    dependencies = {
+      "nvim-tree/nvim-web-devicons",
+    },
+    config = function()
+      require("nvim-tree").setup({})
+    end,
+  },
+  {
+    "christoomey/vim-tmux-navigator",
+    lazy = false, -- Disable lazy loading if experiencing issues
+    cmd = {
+      "TmuxNavigateLeft",
+      "TmuxNavigateDown",
+      "TmuxNavigateUp",
+      "TmuxNavigateRight",
+      "TmuxNavigatePrevious",
+      "TmuxNavigatorProcessList",
+    },
+    keys = {
+      { "<c-h>",  "<cmd><C-U>TmuxNavigateLeft<cr>" },
+      { "<c-j>",  "<cmd><C-U>TmuxNavigateDown<cr>" },
+      { "<c-k>",  "<cmd><C-U>TmuxNavigateUp<cr>" },
+      { "<c-l>",  "<cmd><C-U>TmuxNavigateRight<cr>" },
+      { "<c-\\>", "<cmd><C-U>TmuxNavigatePrevious<cr>" },
+    },
+  },
 
-	-- cmp autocompletion
-	"neovim/nvim-lspconfig",
-	"hrsh7th/cmp-nvim-lsp",
-	"hrsh7th/cmp-buffer",
-	"hrsh7th/cmp-path",
-	"hrsh7th/cmp-cmdline",
-	"hrsh7th/nvim-cmp",
+  -- cmp autocompletion
+  "neovim/nvim-lspconfig",
+  "hrsh7th/cmp-nvim-lsp",
+  "hrsh7th/cmp-buffer",
+  "hrsh7th/cmp-path",
+  "hrsh7th/cmp-cmdline",
+  "hrsh7th/nvim-cmp",
 
-	-- "onsails/lspkind.nvim",
+  -- "onsails/lspkind.nvim",
 
-	-- For luasnip users.
-	"L3MON4D3/LuaSnip",
-	{
-		"saadparwaiz1/cmp_luasnip",
-		dependencies = { "rafamadriz/friendly-snippets" },
-	},
-	-- {
-	-- 	"L3MON4D3/LuaSnip",
-	-- 	-- follow latest release.
-	-- 	version = "v2.*", -- Replace <CurrentMajor> by the latest released major (first number of latest release)
-	-- 	-- install jsregexp (optional!).
-	-- 	build = "make install_jsregexp",
-	-- },
+  -- For luasnip users.
+  "L3MON4D3/LuaSnip",
+  {
+    "saadparwaiz1/cmp_luasnip",
+    dependencies = { "rafamadriz/friendly-snippets" },
+  },
+  -- {
+  -- 	"L3MON4D3/LuaSnip",
+  -- 	-- follow latest release.
+  -- 	version = "v2.*", -- Replace <CurrentMajor> by the latest released major (first number of latest release)
+  -- 	-- install jsregexp (optional!).
+  -- 	build = "make install_jsregexp",
+  -- },
 
-	-- telescope
-	{
-		"nvim-telescope/telescope.nvim",
-		version = "*",
-		dependencies = { "nvim-lua/plenary.nvim" },
-	},
-	{
-		"nvim-telescope/telescope-file-browser.nvim",
-		dependencies = { "nvim-telescope/telescope.nvim", "nvim-lua/plenary.nvim" },
-	},
+  -- telescope
+  {
+    "nvim-telescope/telescope.nvim",
+    version = "*",
+    dependencies = { "nvim-lua/plenary.nvim" },
+  },
+  {
+    "nvim-telescope/telescope-file-browser.nvim",
+    dependencies = { "nvim-telescope/telescope.nvim", "nvim-lua/plenary.nvim" },
+  },
 
-	--commenter
-	{
-		"numToStr/Comment.nvim",
-		config = function()
-			require("Comment").setup()
-		end,
-	},
-	{
-		"windwp/nvim-autopairs",
-		event = "InsertEnter",
-		opts = {}, -- this is equalent to setup({}) function
-	},
-	{ "akinsho/toggleterm.nvim", version = "*", config = true },
-	{ "nvim-treesitter/nvim-treesitter", cmd = "TSUpdate" },
-	{
-		"kylechui/nvim-surround",
-		version = "*", -- Use for stability; omit to use `main` branch for the latest features
-		event = "VeryLazy",
-		config = function()
-			require("nvim-surround").setup({
-				-- Configuration here, or leave empty to use defaults
-			})
-		end,
-	},
-	"famiu/bufdelete.nvim",
-	"jlanzarotta/bufexplorer",
+  --commenter
+  {
+    "numToStr/Comment.nvim",
+    config = function()
+      require("Comment").setup()
+    end,
+  },
+  {
+    "windwp/nvim-autopairs",
+    event = "InsertEnter",
+    opts = {}, -- this is equalent to setup({}) function
+  },
+  { "akinsho/toggleterm.nvim",         version = "*",   config = true },
+  { "nvim-treesitter/nvim-treesitter", cmd = "TSUpdate" },
+  {
+    "kylechui/nvim-surround",
+    version = "*", -- Use for stability; omit to use `main` branch for the latest features
+    event = "VeryLazy",
+    config = function()
+      require("nvim-surround").setup({
+        -- Configuration here, or leave empty to use defaults
+      })
+    end,
+  },
+  "famiu/bufdelete.nvim",
+  "jlanzarotta/bufexplorer",
 
-	{
-		"folke/trouble.nvim",
-		dependencies = { "nvim-tree/nvim-web-devicons" },
-		opts = {
-			-- your configuration comes here
-			-- or leave it empty to use the default settings
-			-- refer to the configuration section below
-		},
-	},
+  {
+    "folke/trouble.nvim",
+    dependencies = { "nvim-tree/nvim-web-devicons" },
+    opts = {
+      -- your configuration comes here
+      -- or leave it empty to use the default settings
+      -- refer to the configuration section below
+    },
+  },
 
-	{
-		"akinsho/flutter-tools.nvim",
-		lazy = false,
-		dependencies = {
-			"nvim-lua/plenary.nvim",
-			"stevearc/dressing.nvim", -- optional for vim.ui.select
-		},
-		config = true,
-	},
-	{
-		"pmizio/typescript-tools.nvim",
-		dependencies = { "nvim-lua/plenary.nvim", "neovim/nvim-lspconfig" },
-		opts = {},
-	},
-	-- mustache
-	"mustache/vim-mustache-handlebars",
-	"vim-test/vim-test",
+  {
+    "akinsho/flutter-tools.nvim",
+    lazy = false,
+    dependencies = {
+      "nvim-lua/plenary.nvim",
+      "stevearc/dressing.nvim", -- optional for vim.ui.select
+    },
+    config = true,
+  },
+  {
+    "pmizio/typescript-tools.nvim",
+    dependencies = { "nvim-lua/plenary.nvim", "neovim/nvim-lspconfig" },
+    opts = {},
+  },
+  -- mustache
+  "mustache/vim-mustache-handlebars",
+  "vim-test/vim-test",
 }, { lockfile = vim.fn.stdpath("state") .. "/lazy-lock.json" })
